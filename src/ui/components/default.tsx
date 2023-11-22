4b941eb2a96e52cd0b38af7d58c9073c9c73a207
VictorPereiira <victorrpsantos@gmail.com>
2023-11-22 11:22:38 -0300

    update

---
+++ src/ui/components/default.tsx
@@ -0,0 +1,32 @@
+import tw from "tailwind-styled-components"
+import { useState, useEffect } from "react";
+
+// Animation
+
+// Icons
+
+// Imgs
+
+// Services & Utils
+import { server } from "@services/index";
+import { util } from "@utils/index"
+
+// Components
+
+export function DefaultComponent({ func }: any) {
+    //  code logic
+
+    return (
+        <Wrapper>
+            {/* content here...*/}
+        </Wrapper >
+    )
+}
+
+// Styles
+const Wrapper = tw.section`  
+    flex 
+    flex-col 
+    flex-grow
+    h-screen
+`
